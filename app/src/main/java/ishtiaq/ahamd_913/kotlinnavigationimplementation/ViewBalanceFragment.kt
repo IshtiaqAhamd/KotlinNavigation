c37f0c7811d1f8e7f0d83b0ea7f27e7415a766a5
Ishtiaq Ahmad <ishtiaq.uaar55@gmail.com>
2022-09-14 23:54:43 +0500

    Navigation In Kotlin

---
+++ app/src/main/java/ishtiaq/ahamd_913/kotlinnavigationimplementation/ViewBalanceFragment.kt
@@ -0,0 +1,10 @@
+package ishtiaq.ahamd_913.kotlinnavigationimplementation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ViewBalanceFragment : Fragment(R.layout.fragment_view_balance)
