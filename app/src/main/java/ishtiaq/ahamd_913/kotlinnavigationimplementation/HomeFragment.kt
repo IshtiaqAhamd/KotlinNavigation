acb873bab473873f3782acf708fe7d7baf27e15f
Ishtiaq Ahmad <ishtiaq.uaar55@gmail.com>
2022-09-18 01:55:29 +0500

    Navigating to Destinations with NavController & SafeArgs

--- app/src/main/java/ishtiaq/ahamd_913/kotlinnavigationimplementation/HomeFragment.kt
+++ app/src/main/java/ishtiaq/ahamd_913/kotlinnavigationimplementation/HomeFragment.kt
@@ -1,10 +1,30 @@
 package ishtiaq.ahamd_913.kotlinnavigationimplementation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
-class HomeFragment : Fragment(R.layout.fragment_home)
+class HomeFragment : Fragment(R.layout.fragment_home){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = findNavController();
+
+        btn_view_balance.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToViewBalanceFragment()
+            navController.navigate(action)
+        }
+
+        btn_view_transaction.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToViewTransactionsFragment()
+            navController.navigate(action)
+        }
+
+        btn_send_money.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToChooseReceiverFragment()
+            navController.navigate(action)
+        }
+    }
+}
